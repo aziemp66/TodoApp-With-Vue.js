0a20d2418c420cc2db309d091753fdb280b5270c
Azie Melza Pratama <aziemp66@gmail.com>
2022-03-01 22:55:46 +0700

    Using Two Way Binding

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,16 @@ const TodosApp = {
     data() {
         return {
             newTodo: "Learn Vue.js",
+            enteredTodoText: "",
         };
+    },
+    methods: {
+        saveTodo(event) {
+            event.preventDefault();
+            this.newTodo = this.enteredTodoText;
+            this.enteredTodoText = "";
+        },
     },
 };
 
-Vue.createApp(TodosApp).mount("#todos-app");
+Vue.createApp(TodosApp).mount("body");
